6ce9d948cfae20d755c43fadb09dd13b74306c56
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-03 20:05:51 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -26,7 +26,7 @@ using StringTools;
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pi,xels (might be less / more in actual pixels depending on your zoom).
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
@@ -78,7 +78,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		var initialState:Class<FlxState> = DisclaimerState; // The FlxState the game starts with.
+		var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 
 		#if !debug
 		//initialState = DisclaimerState;

6aae8014cf576eee5875b254a44a648c6a68e1f9
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-03 19:06:19 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -87,7 +87,6 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -95,7 +94,6 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
 
 		#if html5
 		FlxG.autoPause = false;
